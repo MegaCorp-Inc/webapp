5e4d6f308b9d066ca32aab283d29f8bf630eac67
dongrep <dongre.p@northeastern.edu>
2024-02-06 16:44:44 -0500

    Fixed imports in healthz

--- routes/healthz.js
+++ routes/healthz.js
@@ -1,12 +1,11 @@
 const express = require("express");
 const router = express.Router();
-var bodyParser = require("body-parser");
 const { testDbConnection } = require("../config/db");
 
 const allowedMethods = ["GET"];
 
-router.use(express.json(),(req, res, next) => {
-  if (Object.keys(req.body ?? {}).length > 0) return res.status(400).send();
+router.use(express.json(), (req, res, next) => {
+  if (!req.body || Object.keys(req.body).length > 0) return res.status(400).send();
   if (Object.keys(req.query).length > 0) return res.status(400).send();
   if (!allowedMethods.includes(req.method)) return res.status(405).send();
   res.setHeader("Cache-Control", "no-cache, no-store, must-revalidate");
