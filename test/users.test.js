4176cd97c4427bf51a1e7e959dcff2e3f2b231f2
dongrep <dongre.p@northeastern.edu>
2024-02-09 21:47:43 -0500

    Added tests to test CRU for users

---
+++ test/users.test.js
@@ -0,0 +1,160 @@
+const { describe } = require("mocha");
+const app = require("../app");
+const { checkFields, checkFieldsPresent } = require("../services/users");
+const request = require("supertest");
+
+let assert;
+let expect;
+import("chai")
+  .then((chai) => {
+    assert = chai.assert;
+    expect = chai.expect;
+  })
+  .catch((err) => {
+    console.error("Failed to load chai:", err);
+  });
+
+const user = {
+  first_name: "Piyush",
+  last_name: "Dongre",
+  username: "dongrep@northeastern.edu",
+  password: "asdf@123",
+};
+
+describe("CREATE USER AND RETRIEVE", () => {
+  it("Checks if user object is valid", () => {
+    const result = checkFields(user);
+    assert.equal(result, "Valid");
+  });
+  it("Checks if user object fields are invalid", () => {
+    const result = checkFields({
+      first_name: "",
+      last_name: "",
+      username: "",
+      password: "",
+    });
+    assert.equal(
+      result,
+      "Missing/Invalid: First Name  Last Name  Username  Password field(s)!",
+    );
+  });
+  it("Checks if user object is being created", (done) => {
+    request(app)
+      .post("/v1/user")
+      .send(user)
+      .end((err, res) => {
+        assert.equal(res.status, 201);
+        expect(res.body).to.have.property("id");
+        expect(res.body)
+          .to.have.property("first_name")
+          .and.is.equal(user.first_name);
+        expect(res.body)
+          .to.have.property("last_name")
+          .and.is.equal(user.last_name);
+        expect(res.body)
+          .to.have.property("username")
+          .and.is.equal(user.username);
+        expect(res.body).to.have.property("account_created");
+        expect(res.body).to.have.property("account_updated");
+        done();
+      });
+  });
+  it("Checks if a duplicate user object is being created", (done) => {
+    request(app)
+      .post("/v1/user")
+      .send(user)
+      .end((err, res) => {
+        assert.equal(res.status, 409);
+        done();
+      });
+  });
+  it("Checks if user object was created", (done) => {
+    request(app)
+      .get("/v1/user/self")
+      .set(
+        "Authorization",
+        "Basic " +
+          Buffer.from(user.username + ":" + user.password).toString("base64"),
+      )
+      .end((err, res) => {
+        assert.equal(res.status, 200);
+        expect(res.body).to.have.property("id");
+        expect(res.body)
+          .to.have.property("first_name")
+          .and.is.equal(user.first_name);
+        expect(res.body)
+          .to.have.property("last_name")
+          .and.is.equal(user.last_name);
+        expect(res.body)
+          .to.have.property("username")
+          .and.is.equal(user.username);
+        expect(res.body).to.have.property("account_created");
+        expect(res.body).to.have.property("account_updated");
+        done();
+      });
+  });
+});
+
+describe("UPDATE AND GET USER", () => {
+  const updatedName = "Push";
+  it("Checks if update object fields has username", () => {
+    const result = checkFieldsPresent({
+      username: "asdf"
+    });
+    assert.equal(
+      result,
+      "Username cannot be updated!",
+    );
+  });
+  it("Checks if update object fields are invalid", () => {
+    const result = checkFieldsPresent({
+      first_name: "",
+      last_name: "",
+      password: "",
+    });
+    assert.equal(
+      result,
+      "Missing required fields!",
+    );
+  });
+  it("Checks if user object is being updated", (done) => {
+    request(app)
+      .put("/v1/user/self")
+      .set(
+        "Authorization",
+        "Basic " +
+          Buffer.from(user.username + ":" + user.password).toString("base64"),
+      )
+      .send({ first_name: updatedName })
+      .end((err, res) => {
+        assert.equal(res.status, 204);
+        done();
+      });
+  });
+
+  it("Checks if user object was updated to expected value", (done) => {
+    request(app)
+      .get("/v1/user/self")
+      .set(
+        "Authorization",
+        "Basic " +
+          Buffer.from(user.username + ":" + user.password).toString("base64"),
+      )
+      .end((err, res) => {
+        assert.equal(res.status, 200);
+        expect(res.body).to.have.property("id");
+        expect(res.body)
+          .to.have.property("first_name")
+          .and.is.equal(updatedName);
+        expect(res.body)
+          .to.have.property("last_name")
+          .and.is.equal(user.last_name);
+        expect(res.body)
+          .to.have.property("username")
+          .and.is.equal(user.username);
+        expect(res.body).to.have.property("account_created");
+        expect(res.body).to.have.property("account_updated");
+        done();
+      });
+  });
+});
